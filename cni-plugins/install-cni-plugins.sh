382ba8d29bf78fc03819e1487e4241d0eb147b55
なつき <i@ntk.me>
2021-01-31 12:05:59 -0800

    Support caching in install-cni-plugins.sh

--- cni-plugins/install-cni-plugins.sh
+++ cni-plugins/install-cni-plugins.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
-curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.0/cni-plugins-linux-arm64-v0.9.0.tgz -o /tmp/cni.tgz
-mkdir -p /mnt/data/podman/cni/
-tar xf /tmp/cni.tgz -C /mnt/data/podman/cni/
-rm /tmp/cni.tgz
+if test "$#" -eq 0; then
+  set latest
+fi
+
+set "$(basename "$(curl -fsSLo /dev/null -w "%{url_effective}" https://github.com/containernetworking/plugins/releases/$1)")" "$@"
+
+if ! test -f "/mnt/data/.cache/cni-plugins/cni-plugins-linux-arm64-$1.tgz"; then
+  echo "Downloading https://github.com/containernetworking/plugins/releases/download/$1/cni-plugins-linux-arm64-$1.tgz"
+  curl -fsSLo "/tmp/cni-plugins-linux-arm64-$1.tgz" "https://github.com/containernetworking/plugins/releases/download/$1/cni-plugins-linux-arm64-$1.tgz" \
+    && mkdir -p "/mnt/data/.cache/cni-plugins" \
+    && mv "/tmp/cni-plugins-linux-arm64-$1.tgz" "/mnt/data/.cache/cni-plugins/cni-plugins-linux-arm64-$1.tgz"
+fi \
+  && if [ "$1" != "$2" ]; then
+    ln -sf "cni-plugins-linux-arm64-$1.tgz" "/mnt/data/.cache/cni-plugins/cni-plugins-linux-arm64-$2.tgz"
+  fi
+
+test -f "/mnt/data/.cache/cni-plugins/cni-plugins-linux-arm64-$2.tgz" \
+  && echo "Pouring /mnt/data/.cache/cni-plugins/cni-plugins-linux-arm64-$2.tgz" \
+  && rm -rf /opt/cni/bin \
+  && mkdir -p /opt/cni/bin \
+  && tar -xzC /opt/cni/bin -f "/mnt/data/.cache/cni-plugins/cni-plugins-linux-arm64-$2.tgz"
